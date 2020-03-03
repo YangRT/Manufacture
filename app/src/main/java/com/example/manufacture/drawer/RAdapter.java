e938d185d7f8d86b1ffd13a17ec4ea9180bd209a
YangRT <1946464079@qq.com>
2020-03-03 22:24:22 +0800

    drawer

---
+++ app/src/main/java/com/example/manufacture/drawer/RAdapter.java
@@ -0,0 +1,48 @@
+package com.example.manufacture.drawer;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.manufacture.R;
+
+import java.util.List;
+
+public class RAdapter extends RecyclerView.Adapter<RAdapter.ViewHolder> {
+
+    private List<String> data;
+
+    public RAdapter(List<String> data){
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.item_text);
+        }
+    }
+}
